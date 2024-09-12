54c0be008e55de7e3b89fc34810590e494d388f0
sleepyarya <jjill2479@gmail.com>
2024-09-12 12:43:48 +0700

    first commit

---
+++ siakad3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class siakad3{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String nama, nim;
+        
+
+    }
+}
